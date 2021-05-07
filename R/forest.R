64f4748f8f87d4ff8c9bed6c1fd407838cf0d9f5
Martina Sánchez-Pinillos <martina.sanchezpinillos@gmail.com>
2021-05-07 13:29:32 +0200

    Add artificial forest communities

---
+++ R/forest.R
@@ -0,0 +1,20 @@
+#' @title Post-disturbance dynamics raw data
+#'
+#' @description \code{forest} contains data on the variation of artificial
+#' communities after an hypothetical disturbance.
+#'
+#' @format \code{forest} contains artificial data as it is usually compiled in forest
+#' inventories.
+#' \describe{
+#'    \item{Sites}{Site ID.}
+#'    \item{Survey}{Forest survey corresponding to pre-disturbance (1), disturbed (2),
+#'    and post-disturbance (3) states.}
+#'    \item{BA_site}{Stand basal area for a given survey.}
+#'    \item{Species}{Species ID.}
+#'    \item{BA_sp}{Species basal area for a given site and survey.}
+#'    \item{Cluster}{Forest type ID}
+#' }
+#'
+#'
+
+"forest"
